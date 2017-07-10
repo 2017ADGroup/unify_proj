fc8e4f99552cd6141561ddfa996997ee91d38436
前川諒祐 <2017a_maekawa@axiz.co.jp>
2017-07-10 17:59:35 +0900

    調整中

--- src/dao/ReserveDao.java
+++ src/dao/ReserveDao.java
@@ -11,13 +11,42 @@ import entity.Reserve;
 
 public class ReserveDao {
 
+	private static final String SQL_SELECT_ALL = "SELECT * FROM reserve";
 	private static final String SQL_INSERT = "INSERT INTO reserve (month, day, term, room, purpose, amount, facility, remarks, reserve_host) VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 	private static final String SQL_SELECT_LOGINID_DAYTIME = "SELECT * FROM reserve WHERE login_id=? AND reserve_date=?";
 	private static final String SQL_SELECT_LOGINID_DAYTIME_TERM = "SELECT * FROM reserve WHERE login_id=? AND reserve_date=? AND term=?";
+
 	private Connection connection;
 
 	public ReserveDao(Connection connection) {
 		this.connection = connection;
+	}
+
+	public List<Reserve> selectAll(){
+		try (PreparedStatement stmt = connection.prepareStatement(SQL_SELECT_ALL)) {
+
+			ResultSet rs = stmt.executeQuery();
+			List<Reserve> reserveList = new ArrayList<Reserve>();
+			while (rs.next()) {
+				Reserve reserve = new Reserve(
+					rs.getInt("reserve_id"),
+					rs.getInt("month"),
+					rs.getInt("day"),
+					rs.getInt("term"),
+					rs.getString("room"),
+					rs.getInt("purpose"),
+					rs.getInt("amount"),
+					rs.getString("facility"),
+					rs.getString("Remarks()"),
+					rs.getString("reserve_host")
+					);
+				reserveList.add(reserve);
+				return reserveList;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public void reserveInsert(Reserve reserve){
