f99e3333ac59cfdb049152192da00154b5405119
KatsuyukiAsai <2017a_asai@axiz.co.jp>
2017-07-10 12:06:20 +0900

    エンティティ修正　メールサービス完成

--- src/service/MailService.java
+++ src/service/MailService.java
@@ -1,6 +1,7 @@
 package service;
 
-import com.sun.corba.se.pept.transport.Connection;
+import java.sql.Connection;
+import java.util.List;
 
 import dao.MailDao;
 import entity.Mail;
@@ -8,11 +9,38 @@ import util.DbUtil;
 
 public class MailService {
 
-	public Mail mailSelecto() {
+	public List<Mail> mailSelect() {
 		try (Connection con = DbUtil.getConnection()) {
 			MailDao mailDao = new MailDao(con);
-			Mail mail = mailDao.mailSelect();
-			return mail;
+			return mailDao.mailSelect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public void mailInsert(String to,String from,String time,String subject,String message) {
+		try (Connection con = DbUtil.getConnection()) {
+			MailDao mailDao = new MailDao(con);
+			mailDao.mailInsert(to, from, time, subject, message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void mailUpdate(int id,String to,String from,String time,String subject,String message) {
+		try (Connection con = DbUtil.getConnection()) {
+			MailDao mailDao = new MailDao(con);
+			mailDao.mailupdate(id, to, from, time, subject, message);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Mail mailDelete(int id) {
+		try (Connection con = DbUtil.getConnection()) {
+			MailDao mailDao = new MailDao(con);
+			mailDao.maildelete(id);;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
