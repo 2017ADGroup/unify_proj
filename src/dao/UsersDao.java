adbe1b5f25cbf40ad33d2c0f5b950a8b96977aa2
前川諒祐 <2017a_maekawa@axiz.co.jp>
2017-07-11 14:26:09 +0900

    なるかなああ

--- src/dao/UsersDao.java
+++ src/dao/UsersDao.java
@@ -11,206 +11,205 @@ import entity.Users;
 
 public class UsersDao {
 
-	private static final String SQL_UPDATE = "UPDATE users SET";
-	private static final String SQL_ID_BY_NAME = "SELECT * FROM users WHERE login_id = ?";
-	private static final String SQL_SELECT_ID_AND_PASS = "SELECT * FROM users WHERE login_id = ? AND password = ?";
-	private static final String SQL_DELETE_LOGINID = "DELETE FROM users WHERE login_id = ?";
-	private static final String SQL_SELECT = "SELECT * FROM users";
+		private static final String SQL_UPDATE = "UPDATE users SET";
+		private static final String SQL_ID_BY_NAME = "SELECT * FROM users WHERE login_id = ?";
+		private static final String SQL_SELECT_ID_AND_PASS = "SELECT * FROM users WHERE login_id = ? AND password = ?";
+		private static final String SQL_DELETE_LOGINID = "DELETE FROM users WHERE login_id = ?";
+		private static final String SQL_SELECT = "SELECT * FROM users";
 
-	private Connection connection;
+		private Connection connection;
 
-	public UsersDao(Connection connection) {
-		this.connection = connection;
-	}
-
-	// ログイン
-	public Users findByIDAndPassword(String login_id, String password) {
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_SELECT_ID_AND_PASS)) {
-			stmt.setString(1, login_id);
-			stmt.setString(2, password);
-			ResultSet rs = stmt.executeQuery();
-
-			if (rs.next()) {
-				return new Users(rs.getInt("user_id"), rs.getString("login_id"), rs.getString("password"),
-						rs.getInt("property"), rs.getString("name"), rs.getString("kana"), rs.getInt("authority"), rs.getString("organization"));
-			} else {
-				return null;
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-	}
-
-	public String idByName(String id) {
-		System.out.println(id);
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
-			stmt.setString(1, id);
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				return rs.getString("name");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	public String nameById(String id) {
-		System.out.println(id);
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
-			stmt.setString(1, id);
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				return rs.getString("name");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-
-	//全件取得
-	public List<Users> findAll() {
-		List<Users> list = new ArrayList<Users>();
-
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_SELECT)) {
-			ResultSet rs = stmt.executeQuery();
-
-			while (rs.next()) {
-				Users users = new Users(
-					rs.getInt("user_id"),
-					rs.getString("login_id"),
-					rs.getString("name"),
-					rs.getString("kana")
-
-					);
-				list.add(users);
-			}
-
-
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
-		}
-		return list;
-	}
-
-
-	// ID検索
-	public String find(String id) {
-
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
-
-			stmt.setInt(1, Integer.parseInt(id));
-			ResultSet rs = stmt.executeQuery();
-
-			if (rs.next()) {
-				return rs.getString("name");
-			}
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
+		public UsersDao(Connection connection) {
+			this.connection = connection;
 		}
 
-		return null;
-	}
+		// ログイン
+		public Users findByIDAndPassword(String login_id, String password) {
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_SELECT_ID_AND_PASS)) {
+				stmt.setString(1, login_id);
+				stmt.setString(2, password);
+				ResultSet rs = stmt.executeQuery();
 
-	public int update(Users user) {
-		String sql_update = SQL_UPDATE;
+				if (rs.next()) {
+					return new Users(rs.getInt("user_id"), rs.getString("login_id"), rs.getString("password"),
+							rs.getInt("property"), rs.getString("name"), rs.getString("kana"), rs.getInt("authority"), rs.getString("organization"));
+				} else {
+					return null;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new RuntimeException(e);
+			}
+		}
 
-		Integer id = user.getUser_id();
-		String name = user.getName();
-		String kana = user.getKana();
-		String pass = user.getPassword();
+		public String idByName(String id) {
+			System.out.println(id);
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
+				stmt.setString(1, id);
+				ResultSet rs = stmt.executeQuery();
+				while (rs.next()) {
+					return rs.getString("name");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
 
-		int ptn = 0;
-		if (name != null) {
-			sql_update += " name = ?";
-			ptn = 1;
-			if (kana != null) {
-				sql_update += ", kana = ?";
-				ptn = 2;
+		public String nameById(String id) {
+			System.out.println(id);
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
+				stmt.setString(1, id);
+				ResultSet rs = stmt.executeQuery();
+				while (rs.next()) {
+					return rs.getString("name");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return null;
+		}
+
+
+		//全件取得
+		public List<Users> findAll() {
+			List<Users> list = new ArrayList<Users>();
+
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_SELECT)) {
+				ResultSet rs = stmt.executeQuery();
+
+				while (rs.next()) {
+					Users users = new Users(
+						rs.getInt("user_id"),
+						rs.getString("login_id"),
+						rs.getString("name"),
+						rs.getString("kana")
+
+						);
+					list.add(users);
+				}
+
+
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+			return list;
+		}
+
+
+		// ID検索
+		public String find(String id) {
+
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_ID_BY_NAME)) {
+
+				stmt.setInt(1, Integer.parseInt(id));
+				ResultSet rs = stmt.executeQuery();
+
+				if (rs.next()) {
+					return rs.getString("name");
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+
+			return null;
+		}
+
+		public int update(Users user) {
+			String sql_update = SQL_UPDATE;
+
+			Integer id = user.getUser_id();
+			String name = user.getName();
+			String kana = user.getKana();
+			String pass = user.getPassword();
+
+			int ptn = 0;
+			if (name != null) {
+				sql_update += " name = ?";
+				ptn = 1;
+				if (kana != null) {
+					sql_update += ", kana = ?";
+					ptn = 2;
+					if (pass != null) {
+						sql_update += ", password = ?";
+						ptn = 3;
+					}
+				} else if (pass != null) {
+					sql_update += ", password = ?";
+					ptn = 4;
+				}
+			} else if (kana != null) {
+				sql_update += " kana = ?";
+				ptn = 5;
 				if (pass != null) {
 					sql_update += ", password = ?";
-					ptn = 3;
+					ptn = 6;
 				}
 			} else if (pass != null) {
-				sql_update += ", password = ?";
-				ptn = 4;
-			}
-		} else if (kana != null) {
-			sql_update += " kana = ?";
-			ptn = 5;
-			if (pass != null) {
-				sql_update += ", password = ?";
-				ptn = 6;
-			}
-		} else if (pass != null) {
-			sql_update += " password = ?";
-			ptn = 7;
-		}
-
-		sql_update += " WHERE login_id = ?";
-
-		try (PreparedStatement stmt = connection.prepareStatement(sql_update)) {
-			switch (ptn) {
-			case 1:
-				stmt.setString(1, name);
-				stmt.setInt(2, id);
-				break;
-			case 2:
-				stmt.setString(1, name);
-				stmt.setString(2, kana);
-				stmt.setInt(3, id);
-				break;
-			case 3:
-				stmt.setString(1, name);
-				stmt.setString(2, kana);
-				stmt.setString(3, pass);
-				stmt.setInt(4, id);
-				break;
-			case 4:
-				stmt.setString(1, name);
-				stmt.setString(2, pass);
-				stmt.setInt(3, id);
-				break;
-			case 5:
-				stmt.setString(1, kana);
-				stmt.setInt(2, id);
-				break;
-			case 6:
-				stmt.setString(1, kana);
-				stmt.setString(2, pass);
-				stmt.setInt(3, id);
-				break;
-			case 7:
-				stmt.setString(1, pass);
-				stmt.setInt(2, id);
-				break;
-			default:
-				break;
+				sql_update += " password = ?";
+				ptn = 7;
 			}
 
-			return stmt.executeUpdate();
-		} catch (
+			sql_update += " WHERE login_id = ?";
 
-		SQLException e) {
-			throw new RuntimeException(e);
+			try (PreparedStatement stmt = connection.prepareStatement(sql_update)) {
+				switch (ptn) {
+				case 1:
+					stmt.setString(1, name);
+					stmt.setInt(2, id);
+					break;
+				case 2:
+					stmt.setString(1, name);
+					stmt.setString(2, kana);
+					stmt.setInt(3, id);
+					break;
+				case 3:
+					stmt.setString(1, name);
+					stmt.setString(2, kana);
+					stmt.setString(3, pass);
+					stmt.setInt(4, id);
+					break;
+				case 4:
+					stmt.setString(1, name);
+					stmt.setString(2, pass);
+					stmt.setInt(3, id);
+					break;
+				case 5:
+					stmt.setString(1, kana);
+					stmt.setInt(2, id);
+					break;
+				case 6:
+					stmt.setString(1, kana);
+					stmt.setString(2, pass);
+					stmt.setInt(3, id);
+					break;
+				case 7:
+					stmt.setString(1, pass);
+					stmt.setInt(2, id);
+					break;
+				default:
+					break;
+				}
+
+				return stmt.executeUpdate();
+			} catch (
+
+			SQLException e) {
+				throw new RuntimeException(e);
+			}
 		}
-	}
 
-	public void delete(String login_id) {
+		public void delete(String login_id) {
 
-		try (PreparedStatement stmt = connection.prepareStatement(SQL_DELETE_LOGINID)) {
+			try (PreparedStatement stmt = connection.prepareStatement(SQL_DELETE_LOGINID)) {
 
-			stmt.setString(1, login_id);
+				stmt.setString(1, login_id);
 
-			stmt.executeUpdate();
+				stmt.executeUpdate();
 
-		} catch (SQLException e) {
-			throw new RuntimeException(e);
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+
 		}
-
-	}
-
 }
--- src/servlet/AdminDeleteServlet.java
+++ src/servlet/AdminDeleteServlet.java
@@ -12,11 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import entity.Users;
 import service.UsersService;
 
-
-
-
-
-@WebServlet("/AdminDelete")
+@WebServlet("/adminDelete")
 public class AdminDeleteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
