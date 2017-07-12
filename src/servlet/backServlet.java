f54ea5cf52b6f66f927d4f53f064c45b87ddfac0
kosukeichikawa <2017a_ichikawa@axiz.co.jp>
2017-07-12 17:08:55 +0900

    メニューに戻るできた

--- src/servlet/backServlet.java
+++ src/servlet/backServlet.java
@@ -2,8 +2,8 @@ package servlet;
 
 import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -39,13 +39,14 @@ public class backServlet extends HttpServlet {
 		HttpSession session2 = request.getSession();
 		session2.setAttribute("login_user", login_user);
 		MenuService menuService = new MenuService();
-		// スケジュールのための日付を取得(date型に変更の可能性有)
-		Date date = new Date();
-		String scheduleDay = date.toString();
-		request.setAttribute("schedule",
-				menuService.scheduleCreate((String) session.getAttribute("login_id"), scheduleDay));
 		prepData(request);
-		request.getRequestDispatcher("menu.jsp").forward(request, response);
+		//カレンダー生成過程で生まれた日付を利用してスケジュールを生成
+		String scheduleDay = (int)session2.getAttribute("year") + "-" + (int) session2.getAttribute("month") + "-" + (int)session2.getAttribute("date");
+		request.setAttribute("schedule", menuService.scheduleCreate((String)session2.getAttribute("login_id"), scheduleDay));
+
+	        RequestDispatcher dispatcher = request.getRequestDispatcher("menu.jsp");
+	        //  フォワードによるページ遷移
+	        dispatcher.forward(request, response);
 
 	}
 
@@ -57,72 +58,82 @@ public class backServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
-	private void prepData(HttpServletRequest request) {
-		// 変数初期化
-		int startday;
-		int lastday;
-		// カレンダーの取得
-		Calendar cal = Calendar.getInstance();
-		// 年が設定されていれば、その値を取得。そうでなければ、今年の年号を入れる
-		if (request.getParameter("year") == null) {
-			request.setAttribute("year", cal.get(Calendar.YEAR)); // 現在の年
+	private void prepData(HttpServletRequest request){
+        // 変数初期化
+		HttpSession session = request.getSession();
+        int startday;
+        int lastday;
+        //  カレンダーの取得
+        Calendar cal = Calendar.getInstance();
+        //  年が設定されていれば、その値を取得。そうでなければ、今年の年号を入れる
+        if(request.getParameter("year")==null){
+            session.setAttribute("year", cal.get(Calendar.YEAR));   //  現在の年
 
-		} else {
-			request.setAttribute("year", request.getParameter("year")); // 現在の年
-		}
-		if (request.getParameter("month") == null) {
-			request.setAttribute("month", cal.get(Calendar.MONTH) + 1); // 現在の月
-		} else {
-			request.setAttribute("month", request.getParameter("month")); // 与えらられた月
-		}
-		int year = Integer.parseInt(request.getAttribute("year").toString());
-		int month = Integer.parseInt(request.getAttribute("month").toString());
-		// 月初めの曜日(日-> 1)
-		cal.set(year, month - 1, 1);
-		startday = cal.get(Calendar.DAY_OF_WEEK);
-		// 月末の日付
-		cal.add(Calendar.MONTH, 1);
-		cal.add(Calendar.DATE, -1);
-		lastday = cal.get(Calendar.DATE);
-		// カレンダーのデータを作成する
-		int date = 1;
-		int maxday = 6 * 7;
-		StringBuilder sb = new StringBuilder();
-		sb.append("<table  class=\"table table-bordered\">");
-		sb.append("<tr>");
-		sb.append("<th style=\"color:red;\">日</th>");
-		sb.append("<th>月</th><th>火</th><th>水</th><th>木</th><th>金</th>");
-		sb.append("<th style=\"color:blue;\">土</th>");
-		sb.append("</tr>");
-		sb.append("<tr>");
-		for (int num = 1; num <= maxday; num++) {
-			if (num < startday || num > lastday + startday - 1) {
-				sb.append("<td></td>");
-			} else {
-				sb.append("<td><a href=\"Calender.jsp\" onclick=\"document." + "day" + date
-						+ ".submit(); return false;\">" + date + "</a>" + "<form name=\"day" + date
-						+ "\" action=\"menu\" method=\"get\">" + "<input type=hidden name=\"days\" value=\"" + date
-						+ "\" >" + "</form></td>");
-				date++;
-			}
+        }else{
+            session.setAttribute("year", request.getParameter("year")); //  現在の年
+        }
+        if(request.getParameter("month")==null){
+            session.setAttribute("month", cal.get(Calendar.MONTH)+1);   //  現在の月
+        }else{
+            session.setAttribute("month", request.getParameter("month"));   //  与えらられた月
+        }
+        if(request.getParameter("days")==null){
+            session.setAttribute("date", cal.get(Calendar.DATE)+1);   //  現在の日
+        }else{
+            session.setAttribute("date", Integer.parseInt(request.getParameter("days")));   //  与えらられた日
+        }
+        int year = Integer.parseInt(session.getAttribute("year").toString());
+        int month = Integer.parseInt(session.getAttribute("month").toString());
+        // 月初めの曜日(日-> 1)
+        cal.set(year, month - 1, 1);
+        startday = cal.get(Calendar.DAY_OF_WEEK);
+        // 月末の日付
+        cal.add(Calendar.MONTH, 1);
+        cal.add(Calendar.DATE, -1);
+        lastday = cal.get(Calendar.DATE);
+        //  カレンダーのデータを作成する
+        int date = 1;
+        int maxday = 6 * 7;
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table  class=\"table table-bordered\">");
+        sb.append("<tr>");
+        sb.append("<th style=\"color:red;\">日</th>");
+        sb.append("<th>月</th><th>火</th><th>水</th><th>木</th><th>金</th>");
+        sb.append("<th style=\"color:blue;\">土</th>");
+        sb.append("</tr>");
+        sb.append("<tr>");
+        for (int num = 1; num <= maxday; num++) {
+            if(num < startday || num > lastday + startday - 1){
+                sb.append("<td></td>");
+            }else{
+                sb.append(
+                		"<td><a href=\"Calender.jsp\" onclick=\"document." + "day" +date
+                		+ ".submit(); return false;\">"+ date + "</a>"
+                		+ "<form name=\"day" + date + "\" action=\"menu\" method=\"get\">"
+                		+ "<input type=hidden name=\"days\" value=\"" + date + "\" >"
+                		+ "</form></td>");
+                date++;
+            }
 
-			if (num % 7 == 0) {
-				sb.append("</tr>");
-				if (num > startday + lastday - 1) {
-					break;
-				}
-				if (date < lastday) {
-					sb.append("<tr>");
-				} else {
-					// 最後だったら、ループから抜ける
-					break;
-				}
-			}
-		}
-		sb.append("</table>");
-		// パラメータを設定
-		request.setAttribute("calender", sb);
-		return;
-	}
+
+
+            if(num % 7 == 0){
+                sb.append("</tr>");
+                if(num > startday + lastday - 1){
+                    break;
+                }
+                if(date < lastday){
+                    sb.append("<tr>");
+                }else{
+                    //  最後だったら、ループから抜ける
+                    break;
+                }
+            }
+        }
+        sb.append("</table>");
+        //  パラメータを設定
+        request.setAttribute("calender", sb);
+        return;
+    }
 
 }
